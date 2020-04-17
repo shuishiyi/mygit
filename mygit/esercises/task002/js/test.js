685a4df77a051d8b8cb1932a1245102ed6d7fc11
292301951 <292301951@qq.com>
2020-04-18 04:36:17 +0800

    练习

---
+++ mygit/esercises/task002/js/test.js
@@ -0,0 +1,13 @@
+function oArr(arr){
+    var array = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (array .indexOf(arr[i]) === -1) {
+            array .push(arr[i])
+        }
+    }
+    return array;
+}
+
+
+var a = "1,2,3";
+console.log(split(a));
